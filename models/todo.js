23babb5adb1d17bb21c9d4226fdc4503da378f29
haison1234 <sonnguyen556699@gmail.com>
2026-02-01 19:35:48 +0700

    Add backend source code

---
+++ models/todo.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+ }
+);
+
+module.exports = mongoose.model("Todo", todoSchema, "todos");
